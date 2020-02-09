3be440e3599aa6cf2ec7796beb13959d764f7df8
Ken Boyd <klb417@gmail.com>
2020-02-09 12:34:22 -0600

    completed chapter on posting

--- libraryapp/views/libraries/list.py
+++ libraryapp/views/libraries/list.py
@@ -1,5 +1,6 @@
 import sqlite3
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from libraryapp.models import Library, model_factory
 from ..connection import Connection
@@ -27,3 +28,22 @@ def library_list(request):
         context = {"all_libraries": all_libraries}
 
         return render(request, template, context)
+
+    if request.method == "POST":
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute(
+                """
+            INSERT INTO libraryapp_library
+            (
+                title, address
+            )
+            VALUES (?, ?)
+            """,
+                (form_data["title"], form_data["address"],),
+            )
+
+        return redirect(reverse("libraryapp:libraries"))
